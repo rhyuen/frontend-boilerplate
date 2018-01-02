40a41102c6dee57e327e2a0096692cbd607279fa
Robert Yuen <dungeonry@hotmail.com>
2018-01-01 17:45:53 -0800

    First commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+
+class App extends Component{
+
+    render(){
+        return (
+            <div className = "root">   
+                hi, front end boiler plate is here.            
+            </div>
+        )
+    }
+}
+
+export default App;
