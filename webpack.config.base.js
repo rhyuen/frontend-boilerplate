5e4ab36440c56ee69ea80ef688095e4b0d37f2f8
Robert Yuen <dungeonry@hotmail.com>
2019-02-11 05:20:10 -0800

    updates for react hooks and there were some issues with misnamed files and them not being capitalized.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -7,7 +7,7 @@ module.exports = {
         "index": path.join(__dirname, "src/Index.jsx")
     },
     output: {
-        path: path.join(__dirname, "public"),
+        path: path.join(__dirname, "dist"),
         filename: "bundle.js"
     },
     module: {
